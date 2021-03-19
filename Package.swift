7193170535f6067a7524416fc6de38af18f1c1ee
Kristiina Rahkema <kristiinara@gmail.com>
2021-03-19 16:25:27 +0200

    Renamed application to GraphifyEvolution

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "GraphifySwiftEvolution",
+    name: "GraphifyEvolution",
     platforms: [
           .macOS(.v10_14),
        ],
@@ -21,7 +21,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "GraphifySwiftEvolution",
+            name: "GraphifyEvolution",
             dependencies: [
                 .product(name: "SourceKittenFramework", package: "SourceKitten"),
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
